6939c1128ea25ed932fa1b65878a45d8cff8439d
Deniss Muhļa <deniss.muhla@gmail.com>
2019-10-07 12:15:45 +0300

    Completes ExRatesTable test

--- src/app/ex-rates/ex-rates-table.tsx
+++ src/app/ex-rates/ex-rates-table.tsx
@@ -7,29 +7,37 @@ type ExRatesTableProps = {
 };
 
 const ExRatesTable: FunctionComponent<ExRatesTableProps> = ({ data }) => {
-    return data && data.rates ? (
-        <table>
-            <thead>
-                <tr>
-                    <th>{'currency'}</th>
-                    <th>{'rate'}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Object.keys(data.rates).map(key => {
-                    return (
-                        <tr key={key}>
-                            <td>{key}</td>
-                            <td>{data.rates[key]}</td>
+    return (
+        <div data-test={ExRatesTable.displayName}>
+            {data && data.rates ? (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>{'currency'}</th>
+                            <th>{'rate'}</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    ) : (
-        <span>{'No data'}</span>
+                    </thead>
+                    <tbody>
+                        {Object.keys(data.rates).map(key => {
+                            return (
+                                <tr key={key} data-test={`row-${key}`}>
+                                    <td data-test={'curr'}>{key}</td>
+                                    <td data-test={'rate'}>
+                                        {data.rates[key]}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            ) : (
+                <span data-test={'no-data'}>{'No data'}</span>
+            )}
+        </div>
     );
 };
+
+ExRatesTable.displayName = 'ExRatesTable';
 
 // use API status component HOC
 export default withApiStatus(ExRatesTable);
--- src/app/ex-rates/ex-rates.spec.js
+++ src/app/ex-rates/ex-rates.spec.js
@@ -1,5 +1,8 @@
 const EX_RATES_API = 'https://api.exchangeratesapi.io/latest';
 const GET_RATES_BUTTON_QUERY = '[data-test=GetRates-button]';
+const GET_RATES_ROW_QUERY = key => `[data-test=row-${key}]`;
+const GET_RATES_ROW_CURR_QUERY = '[data-test=curr]';
+const GET_RATES_ROW_RATE_QUERY = key => '[data-test=rate]';
 const REDUX_EX_RATES_SELECTOR = win =>
     win.store.getState().api.exRates.latestRates;
 
@@ -8,7 +11,7 @@ context('Exchange Rates', () => {
         cy.whitelist();
     });
 
-    it('Clicks get exchange rates button', () => {
+    it('Clicks get exchange rates button and render data', () => {
         // Add API request watcher
         cy.route({
             method: 'GET',
@@ -22,7 +25,7 @@ context('Exchange Rates', () => {
         cy.get(GET_RATES_BUTTON_QUERY).should('exist');
         cy.get(GET_RATES_BUTTON_QUERY).click();
 
-        // wait for API request
+        // wait for API response
         cy.wait('@apiExRates').then(xhr => {
             const responseData = xhr.response.body;
             assert.isNotNull(responseData, 'Get ExRates API call has data');
@@ -35,6 +38,18 @@ context('Exchange Rates', () => {
                         responseData
                     );
                 });
+
+            // test rendered table
+            Object.keys(responseData.rates).forEach(key => {
+                cy.get(GET_RATES_ROW_QUERY(key)).then(row => {
+                    cy.wrap(row)
+                        .children(GET_RATES_ROW_CURR_QUERY)
+                        .should('contain', key);
+                    cy.wrap(row)
+                        .children(GET_RATES_ROW_RATE_QUERY)
+                        .should('contain', responseData.rates[key]);
+                });
+            });
         });
     });
 });
